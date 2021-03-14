94344b6e33e07d5890ba127e3b0f256b9b8556b6
yaroslavas2001 <61469500+yaroslavas2001@users.noreply.github.com>
2021-03-14 16:57:18 +0200

    попытка обращаться к элементам

--- chapter-2/dist/index.js
+++ chapter-2/dist/index.js
@@ -1,18 +1,27 @@
 "use strict";
 var _a;
-console.log('Hello TypeScript!');
-let a = 1 + 2;
-let b = a + 3;
-let c = {
-    apple: a,
-    banana: b
-};
-let d = c.apple * 4;
-let list = [];
-list.push("a");
-list.push(1);
-list.push(true);
-console.log(list);
 let t = (_a = document.getElementById("name")) === null || _a === void 0 ? void 0 : _a.innerHTML;
 console.log(t);
+let element = document.getElementsByTagName("body")[0];
+let table = document.createElement("div");
+table.className = "table";
+let row = document.createElement("div");
+row.className = "row";
+let cell = document.createElement("div");
+let cellText = document.createTextNode("клетка");
+cell.appendChild(cellText);
+cell.className = "cell";
+row.appendChild(cell);
+let cell1 = document.createElement("div");
+let cellText1 = document.createTextNode("клетка 1");
+cell1.appendChild(cellText1);
+cell1.className = "cell";
+row.appendChild(cell1);
+let cell2 = document.createElement("div");
+let cellText2 = document.createTextNode("клетка 2");
+cell2.appendChild(cellText2);
+cell2.className = "cell";
+row.appendChild(cell2);
+table.appendChild(row);
+element.appendChild(table);
 //# sourceMappingURL=index.js.map
--- chapter-2/src/index.ts
+++ chapter-2/src/index.ts
@@ -1,16 +1,26 @@
-console.log('Hello TypeScript!')
-let a = 1 + 2
-let b = a + 3
-let c =
-{
- apple: a,
- banana: b
-}
-let d = c.apple * 4;
-let list=[];
-list.push("a")
-list.push(1)
-list.push(true)
-console.log(list)
 let t = document.getElementById("name")?.innerHTML;
 console.log(t)
+let element = document.getElementsByTagName("body")[0];
+let table = document.createElement("div");
+table.className = "table";
+let row = document.createElement("div");
+row.className = "row";
+
+let cell= document.createElement("div");
+let cellText = document.createTextNode("клетка");
+cell.appendChild(cellText);
+cell.className = "cell";
+row.appendChild(cell);
+let cell1= document.createElement("div");
+let cellText1 = document.createTextNode("клетка 1");
+cell1.appendChild(cellText1);
+cell1.className = "cell";
+row.appendChild(cell1);
+let cell2= document.createElement("div");
+let cellText2 = document.createTextNode("клетка 2");
+cell2.appendChild(cellText2);
+cell2.className = "cell";
+row.appendChild(cell2);
+
+table.appendChild(row);
+element.appendChild(table);
